96bcf7efe7b79af4eb71821a66719f4959dda746
venkatvr123 <venkatvr897@gmail.com>
2019-12-26 22:06:30 +0530

    Updates

---
+++ app/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './App.css'
+import SliderPage from '../../src'
+
+function App() {
+  return (
+    <SliderPage>
+      <section id="html">
+        <div>HTML</div>
+      </section>
+      <section id="css">
+        <div>CSS</div>
+      </section>
+      <section id="js">
+        <div>JavaScript</div>
+      </section>
+      <section id="react">
+        <div>React</div>
+      </section>
+      <section id="vue">
+        <div>Vue</div>
+      </section>
+    </SliderPage>
+  )
+}
+
+export default App
